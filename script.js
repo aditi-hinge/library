7fe74b5ac8451c3950f596373ec15d4e15a76973
aditi-hinge <aditi.hinge@gmail.com>
2022-03-03 20:23:10 +0900

    basic form popout JS

---
+++ script.js
@@ -0,0 +1,45 @@
+let close = document.getElementById("cross");
+let popupForm = document.querySelector(".popup-form");
+let addBookButton = document.getElementById("add-book-button");
+let bookName = document.getElementById("book-name");
+let author = document.getElementById("author");
+let pages = document.getElementById("pages");
+let read = document.getElementById("read");
+let saveButton = document.getElementById("save-button");
+
+// close pop-up from
+close.addEventListener("click",()=>{
+  popupForm.style.display = "none";
+})
+
+//open pop-up form
+addBookButton.addEventListener("click", ()=>{
+  popupForm.style.display = "flex";
+})
+
+//get value from form
+saveButton.addEventListener("click", ()=>{
+  console.log(bookName.value + author.value + pages.value + read.value);
+})
+
+
+
+let myLibrary = [];
+
+function Book(title,author,pages,read) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function(){
+  return `${title} by ${author}, ${pages} pages, ${read}.`;
+}
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
+book1.info();
