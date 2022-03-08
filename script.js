8c5c1e5933c683285068213490d812f18b752301
aditi-hinge <aditi.hinge@gmail.com>
2022-03-08 21:25:59 +0900

    final framework

--- script.js
+++ script.js
@@ -6,11 +6,8 @@ let author = document.getElementById("author");
 let pages = document.getElementById("pages");
 let read = document.getElementById("read");
 let saveButton = document.getElementById("save-button");
-// let showBooks = document.querySelector(".show-books");
 let showBooks = document.getElementById("show-books");
-// let name = document.getElementById("name");
-
-
+let form = document.getElementById("form");
 
 let myLibrary = [];
 
@@ -21,70 +18,108 @@ close.addEventListener("click",()=>{
 
 //open pop-up form
 addBookButton.addEventListener("click", ()=>{
-  // popupForm.style.display = "flex";
-  addBookToLibrary();
+  popupForm.style.display = "flex";
+  bookName.value = "";
+  author.value = "";
+  pages.value = "";
 })
 
 //get value from form
 saveButton.addEventListener("click", ()=>{
-//  Book();
+    if(bookName.value === "" || author.value === "" ||
+    pages.value === "") return;
+    popupForm.style.display = "none";
 })
 
-//function to makes array out of user input
-let makeArray =()=>{
-  myLibrary.push(bookName.value,author.value,pages.value);
-  let title = myLibrary[0];
-  return title;
-  // Book(title);
-  // console.log(title);
+form.addEventListener("submit", (event)=>{
+  addBookToLibrary();
+  event.preventDefault();
+})
+
+
+function addBookToLibrary() {
+
+  let brandNewBook = new Book(bookName.value, author.value, pages.value, read.checked);
+  console.log(brandNewBook);
+
+  myLibrary.push(brandNewBook);
+  console.log(myLibrary);
+  
+
+  // do stuff here
+  let card = document.createElement("div");
+  let titleSpace = document.createElement("h3");
+  let authorSpace = document.createElement("p");
+  let pagesSpace = document.createElement("p");
+  let readButton = document.createElement("button");
+  let removeButton = document.createElement("button");
+
+  showBooks.appendChild(card);
+  card.classList.add("card");
+
+  card.setAttribute("id", myLibrary.length-1);
+  removeButton.setAttribute("id", myLibrary.length-1);
+  readButton.setAttribute("id", myLibrary.length-1);
+
+  card.append(titleSpace);
+  card.append(authorSpace);
+  card.append(pagesSpace);  
+  card.append(readButton);
+  card.append(removeButton);
+
+
+  
+
+  titleSpace.innerHTML = brandNewBook.title;
+  authorSpace.innerHTML = brandNewBook.author;
+  pagesSpace.innerHTML = brandNewBook.pages;
+  removeButton.innerHTML = "Delete";
+
+  //for read button
+  if(read.checked === true){
+    readButton.innerHTML = "read";
+  } else if(read.checked === false) {
+    readButton.innerHTML = "unread";
+  } else {
+    return "error";
+  }
+ 
+  
+  removeButton.addEventListener("click",(event)=>{
+    showBooks.removeChild(card);
+    myLibrary.splice(event.target.parentNode.id, 1);
+  })
+  
+  //read button toggle
+  readButton.addEventListener("click", (event)=>{
+    if(readButton.innerHTML === "read"){
+      readButton.innerHTML = "unread";
+    } else if(readButton.innerHTML === "unread"){
+      readButton.innerHTML = "read";
+    } else {
+      return "error";
+    }
+  //change in the library
+  if(myLibrary[event.target.parentNode.id].read === true){
+    myLibrary[event.target.parentNode.id].read = false;
+  } else if(myLibrary[event.target.parentNode.id].read === false){
+    myLibrary[event.target.parentNode.id].read = true;
+  } else {
+    return "error";
+  }
+ 
 }
-
-// function Book() {
-//   makeArray(title);
-//   // the constructor...
-//   this.title = makeArray(title);
-//   // this.author = author;
-//   // this.pages = pages;
-//   // this.read = read;
-//   this.info = function(){
-//   console.log(title);
-// }
-// }
+ 
+ 
+)
+};
 
 
-function Book(title,author,pages,read) {
-  // the constructor...
+
+
+function Book(title, author,pages, read) {    
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function(){
-  return title, author, pages, read;
 }
-}
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
-function addBookToLibrary() {
-  // do stuff here
-  let card = document.createElement("div");
-  showBooks.appendChild(card);
-  card.style.height = "250px";
-  card.style.width = "250px";
-  card.style.borderRadius = "10px";
-  card.style.backgroundColor = "white";
-  card.style.color = "black";
-  // card.style.display = "grid";
-  Book();
-  myLibrary.push(book1);
-  // console.log(myLibrary)
-}
-
-
-// for(let info in Book) alert(info);
-
-// console.log(typeof(book1));
-// console.log(book1.length)
-// for(let i=0; i<myLibrary.length; i++){
-//   console.log("wee");
-// }
-
-
